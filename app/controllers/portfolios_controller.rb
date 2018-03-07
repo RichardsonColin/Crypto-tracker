8391a95f0cc420e12783cac2dc820f267a162fd0
Colin Richardson <richardsonjcolin@gmail.com>
2018-03-07 22:24:24 +0000

    adds the method and the html to show the created portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,11 +1,20 @@
 class PortfoliosController < ApplicationController
+  def show
+    @portfolio = Portfolio.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    @portfolio = Portfolio.new(params[:portfolio])
+    @portfolio = Portfolio.new(portfolio_params)
 
     @portfolio.save
     redirect_to @portfolio
   end
 end
+
+private
+  def portfolio_params
+    params.require(:portfolio).permit(:portfolio_name, :description)
+  end
