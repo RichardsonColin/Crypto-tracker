8865eac237529f4d6d645be3cad055385afb39d5
Colin Richardson <richardsonjcolin@gmail.com>
2018-03-07 22:14:52 +0000

    updates create method to use the Portfolio model to save to database

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,11 @@
 class PortfoliosController < ApplicationController
   def new
   end
+
+  def create
+    @portfolio = Portfolio.new(params[:portfolio])
+
+    @portfolio.save
+    redirect_to @portfolio
+  end
 end
