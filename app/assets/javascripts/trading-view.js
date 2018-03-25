cfeb4d3efb8cdf87a0e15a0178510b90635ad09f
Colin Richardson <richardsonjcolin@gmail.com>
2018-03-25 22:37:28 +0000

    adds a tradingview ticker to top of pages

---
+++ app/assets/javascripts/trading-view.js
@@ -0,0 +1,10 @@
+// $(document).on('turbolinks:load', function() {
+//   console.log('AJAX')
+//   $.ajax({
+//       url: "https://s3.tradingview.com/external-embedding/embed-widget-tickers.js",
+//       cache: false,
+//       success: function(html){
+//         $(".tradingview-widget-container__widget").append(html);
+//       }
+//   });
+// });
